88345e41ec5d0c0679d4742ec855320012ef4aa1
David Asabina <david@supr.nu>
2014-11-22 14:40:28 +0100

    Offlineimap settings for my personal mailbox

---
+++ offlineimap.py
@@ -0,0 +1,17 @@
+from subprocess import Popen, PIPE
+from os import path
+
+def get_email_password(account):
+  if ("%s" % account).isalpha():
+    f = path.expanduser("~/.mutt/%s.gpg" % (account))
+    s = "gpg --quiet --no-tty --for-your-eyes-only -d %s" % (f)
+  
+    p = Popen(s.split(' '), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    return output
+
+  else:
+    return None
+
+print "password is %s" % (get_email_password("vidbina"))
+print "password is %s" % (get_email_password(12))
